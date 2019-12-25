15579765fde7544dd0a76fa224f7ddc687f5fd20
Soumya Deb <debloper@gmail.com>
2019-12-26 02:16:55 +0530

    Generate JSON instead of text file
    - Doesn't deal with character validity etc.
    - Doesn't check for JSON validity either
    - Overall pretty shitty script... 3/10

--- list-daemons.sh
+++ list-daemons.sh
@@ -2,22 +2,34 @@
 
 # Description string identifier
 desc="Description="
+fout="daemons.json"
 
 # Create file to save output to
-touch list-daemons.txt
+touch $fout
+
+# Initiate JSON structure
+echo "{" > $fout
 
 # Iterate over the systemd units
-for file in /lib/systemd/system/*
+units=(/lib/systemd/system/*)
+for file in "${units[@]}"
 do
   if [[ -f $file ]]; then
+    # Do not prepend comma for the first item
+    if [ ${units[0]} != $file ]; then
+      echo "," >> $fout
+    fi
     # Capture file name
     daemon="${file##*/}"
     # Extract daemon description
     description=`grep $desc $file | cut -d'=' -f 2`
     # Format a readable (and parsable) output
-    echo "$daemon: $description" >> list-daemons.txt
+    echo -n "\"$daemon\": \"$description\"" >> $fout
   fi
 done
 
+# End JSON structure
+echo -e "\n}" >> $fout
+
 # Notify user about completion of the process
-echo "List of daemons on this system are written to ./list-daemons.txt"
+echo "List of daemons on this system are written to ./daemons.json"
